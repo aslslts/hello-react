2e3e5366a06ac7c49e3ea26275981c055f018e74
Aslı Sulutaş <cemree.1402@gmail.com>
2022-11-02 14:02:13 +0300

    Components and props added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <User
+        title="Kişi"
+        data={{ name: "Asli Sulutas", city: "Mersin", age: 25 }}
+        friends={["Asli", "Murat", "Fatma", "Ahmet"]}
+      />
     </div>
   );
 }
